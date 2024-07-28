91ebe6a7c9d533eb504ceb45abe7775e2280d3e1
Chuyue Zhang <irondumpling233@outlook.com>
2024-07-28 18:21:43 -0400

    add views

--- Assets/Scripts/InGame/BattleFields/Chariots/Tower.cs
+++ Assets/Scripts/InGame/BattleFields/Chariots/Tower.cs
@@ -5,8 +5,29 @@ using System.Threading.Tasks;
 
 namespace InGame.BattleFields.Chariots
 {
+    public enum SeekMode
+    {
+        None,
+        Nearest,
+    }
+
     public class Tower
     {
-        
+        private Bullet m_bullet;
+        private UnlimitedProperty m_attack;
+        private UnlimitedProperty m_bulletCount;
+        private SeekMode m_seekMode;
+
+        public Tower()
+        {
+
+        }
+
+        public void GenerateBullet()
+        {
+            
+        }
     }
+
+
 }
---
+++ Assets/Scripts/InGame/Views/BulletView.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace InGame.Views
+{
+    public class BulletView : MonoBehaviour
+    {
+        
+    }
+}
