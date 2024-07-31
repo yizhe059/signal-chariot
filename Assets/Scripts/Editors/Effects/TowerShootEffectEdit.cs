5778dcfbb7979ad734e8f6a12b19d6897acc71e7
Winnerrang <leo.hanxu@mail.utoronto.ca>
2024-07-30 22:48:24 -0400

    add tower effect

---
+++ Assets/Scripts/Editors/Effects/TowerShootEffectEdit.cs
@@ -0,0 +1,12 @@
+﻿using InGame.Effects;
+
+namespace Editors.Effects
+{
+    public class TowerShootEffectEdit: EffectEdit
+    {
+        public override Effect CreateEffect()
+        {
+            return TowerShootEffect.CreateEffect();
+        }
+    }
+}
