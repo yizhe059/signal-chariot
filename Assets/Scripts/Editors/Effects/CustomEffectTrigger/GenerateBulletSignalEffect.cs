6834dd9771b05306708e8f6b4f6b7b213b4319f1
Winnerrang <leo.hanxu@mail.utoronto.ca>
2024-08-24 19:20:50 -0400

    Add Bullet Module

---
+++ Assets/Scripts/Editors/Effects/CustomEffectTrigger/GenerateBulletSignalEffect.cs
@@ -0,0 +1,17 @@
+﻿using InGame.Boards.Signals;
+using InGame.Effects;
+using UnityEngine;
+
+namespace Editors.Effects.CustomEffectTrigger
+{
+    public class GenerateBulletSignalEffect: EffectEdit
+    {
+        public SignalType type;
+        [Min(1)]
+        public int numOfBullets;
+        public override Effect CreateEffect()
+        {
+            return InGame.Effects.EffectElement.GenerateBulletSignalEffect.CreateEffect(type, numOfBullets);
+        }
+    }
+}
---
+++ Assets/Scripts/InGame/BoardEffects/EffectElement/GenerateBulletSignalEffect.cs
@@ -0,0 +1,45 @@
+﻿using InGame.Boards.Signals;
+using InGame.Cores;
+using SetUps;
+
+namespace InGame.Effects.EffectElement
+{
+    public class GenerateBulletSignalEffect: Effect
+    {
+        public SignalType type;
+        public int numOfBullets;
+        
+        public override void OnTrigger(EffectBlackBoard blackBoard)
+        {
+            var signalController = GameManager.Instance.GetSignalController();
+            var orientation = m_module.orientation;
+            signalController.CreateSignal(new SignalSetUp
+            {
+                dir = Signal.OrientationToDirection(orientation, Signal.Direction.Up),
+                energy = numOfBullets,
+                pos = blackBoard.slot.pos
+            }, 0, type);
+        }
+
+        public override Effect CreateCopy()
+        {
+            return new GenerateBulletSignalEffect
+            {
+                type = type,
+                numOfBullets = numOfBullets
+            };
+        }
+
+        public static Effect CreateEffect(SignalType type, int numOfBullets)
+        {
+            return new GenerateBulletSignalEffect
+            {
+                type = type,
+                numOfBullets = numOfBullets
+            };
+            
+        }
+        
+        
+    }
+}
