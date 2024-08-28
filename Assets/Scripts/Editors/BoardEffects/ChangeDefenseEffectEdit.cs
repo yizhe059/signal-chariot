ec45042f8b8238983402f05e76e6058ec6743171
Winnerrang <leo.hanxu@mail.utoronto.ca>
2024-08-27 23:53:35 -0400

    Add Change Defense Effect

---
+++ Assets/Scripts/Editors/BoardEffects/ChangeDefenseEffectEdit.cs
@@ -0,0 +1,15 @@
+﻿using InGame.Effects;
+using InGame.Effects.EffectElement;
+
+namespace Editors.Effects
+{
+    public class ChangeDefenseEffectEdit: EffectEdit
+    {
+        public int delta = 0;
+        
+        public override Effect CreateEffect()
+        {
+            return ChangeDefenseEffect.CreateEffect(delta);
+        }
+    }
+}
