8262df748f7171100818c0d3f58c7bd709570c12
Winnerrang <leo.hanxu@mail.utoronto.ca>
2024-09-15 23:27:58 -0400

    Add ElectricChargeGainPerTrigger parameter

--- Assets/Scripts/Editors/BoardEffects/WeaponShootEffectEdit.cs
+++ Assets/Scripts/Editors/BoardEffects/WeaponShootEffectEdit.cs
@@ -14,11 +14,12 @@ namespace Editors.Effects
         public float heatCostPerShot;
         [Min(0f)][Tooltip("电荷容量。电荷容量为0时代表永远为打出第二级子弹")]
         public int electricCapacity;
-        
+        [Min(0f)][Tooltip("每次触发时，攒多少点电荷")]
+        public int electricChargeGainPerTrigger = 1;
         public override Effect CreateEffect()
         {
             if (heatGauge < 0.001f) heatGauge = 0.001f;
-            return WeaponShootEffect.CreateEffect(heatGauge, dissipationRate, heatCostPerShot, electricCapacity);
+            return WeaponShootEffect.CreateEffect(heatGauge, dissipationRate, heatCostPerShot, electricCapacity, electricChargeGainPerTrigger);
         }
     }
 }
